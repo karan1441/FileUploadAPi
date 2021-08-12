6e3296516cad943c31ed5f2ddc0fe38324603392
Chandrasekaran <53594647+karan1441@users.noreply.github.com>
2021-08-12 07:15:53 +0200

    Add files via upload

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    
+    public class UploadController : ApiController
+    {
+        private const string Container = "ocpuploaddrawio";
+
+        
+        public async Task<IHttpActionResult> UploadFile()
+        {
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var accountName = ConfigurationManager.AppSettings["storage:account:name"];
+            var accountKey = ConfigurationManager.AppSettings["storage:account:key"];
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer ocpuploaddrawio = blobClient.GetContainerReference(Container);
+            var provider = new AzureStorageMultipartFormDataStreamProvider(ocpuploaddrawio);
+
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+            // Retrieve the filename of the file you have uploaded
+            var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+            if (string.IsNullOrEmpty(filename))
+            {
+                return BadRequest("An error has occured while uploading your file. Please try again.");
+            }
+
+            return Ok($"File: {filename} has successfully uploaded");
+        }
+    }
+}
